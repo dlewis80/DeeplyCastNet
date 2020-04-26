228459ac7c88cd6e77a874db7c946a105d0bf807
Matthew Woodward <mwoodward@gatech.edu>
2020-04-25 22:29:03 -0500

    Update pc app with correct settings options and message structure

--- pcApp/Form1.cs
+++ pcApp/Form1.cs
@@ -16,6 +16,9 @@ namespace AcousticModem
     {
 
         public StringBuilder RxMessage;
+        public bool messageRecieved;
+        public bool sendingSettings;
+
         public AcousticModemForm()
         {
             InitializeComponent();
@@ -23,15 +26,11 @@ namespace AcousticModem
 
             BaudSelect.DataSource = new int[] {110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200};
             BaudSelect.SelectedIndex = 6;
-        }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
+            TotalTextBox.Text = "3";
 
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
+            messageRecieved = false;
+            sendingSettings = false;
 
         }
 
@@ -64,7 +63,7 @@ namespace AcousticModem
         private void sendSerial() {
             // Need to add error checking for connection
             // Also need to check that message is in the right format
-            serialPort1.WriteLine(messageTextBox.Text);
+            serialPort1.WriteLine("1"+messageTextBox.Text);
             logTextBox.AppendText("Tx: " + messageTextBox.Text + "\n");
             messageTextBox.Clear();
         }
@@ -76,18 +75,28 @@ namespace AcousticModem
 
         private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
-            RxMessage.Append(serialPort1.ReadExisting());
+            string existing = serialPort1.ReadExisting();
 
-            String received = RxMessage.ToString();
-            RxMessage.Clear();
-            RxMessage.Append(received); //This isn't how you are supposed to use StringBuilders lol
-
-            System.Diagnostics.Debug.WriteLine("received: " + received + " from source");
-
-            if (received.EndsWith("\n"))
+            if (sendSettings && existing == "0")
             {
-                logTextBox.AppendText("Rx: " + received);
+                messageRecieved = true;
+                return;
+            }
+            else
+            {
+                RxMessage.Append(existing);
+
+                String received = RxMessage.ToString();
                 RxMessage.Clear();
+                RxMessage.Append(received); //This isn't how you are supposed to use StringBuilders lol
+
+                System.Diagnostics.Debug.WriteLine("received: " + received + " from source");
+
+                if (received.EndsWith("\n"))
+                {
+                    logTextBox.AppendText("Rx: " + received);
+                    RxMessage.Clear();
+                }
             }
         }
 
@@ -107,47 +116,78 @@ namespace AcousticModem
             }
         }
 
-        private void logTextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TotalTextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TargetLabel_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void A3TextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void A3Label_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void messageTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             // Catch the enter key
             if (e.KeyChar == '\r')
             {
                 sendSerial();
+            }
+        }
+
+        private void SendSettingsButton_Click(object sender, EventArgs e)
+        {
+
+            sendingSettings = true;
+
+            bool success = true;
+            success &= SendSetting('0', TotalTextBox.Text);
+            success &= SendSetting('1', IdComboBox.SelectedItem.ToString());
+            success &= SendSetting('2', Target1Combo.SelectedItem.ToString());
+            success &= SendSetting('3', Target2Combo.SelectedItem.ToString());
+            success &= SendSetting('4', BinCombo.SelectedItem.ToString());
+            success &= SendSetting('5', EnergyCombo.SelectedItem.ToString());
+            success &= SendSetting('6', GainCombo.SelectedItem.ToString());
+            success &= SendSetting('7', A1TextBox.Text);
+            success &= SendSetting('8', A2TextBox.Text);
+            success &= SendSetting('9', A3TextBox.Text);
+            sendingSettings = false;
+
+            if (!success)
+            {
+                //display an error somehow
+            }
+        }
+
+        private bool SendSetting(char code, string data)
+        {
+
+            if (data == "")
+            {
+                return false;
+            }
+
+            StringBuilder TxMessage = new StringBuilder();
+            TxMessage.Append("1");
+            TxMessage.Append(code);
+            TxMessage.Append(data);
+            //string toSend = TxMessage.ToString();
+            string toSend = "1" + code + data;
+
+            System.Diagnostics.Debug.WriteLine("Transmitting: ", toSend, "\r\n");
+
+            messageRecieved = false;
+            int timeout = 10;
+            while (!messageRecieved)
+            {
+                serialPort1.WriteLine(toSend);
+
+                if (timeout <= 0)
+                {
+                    // Failed to recieve response
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void IdComboBox_DropDown(object sender, EventArgs e)
+        {
+            IdComboBox.Items.Clear();
+            for (int i = 0; i < (Int16.Parse(TotalTextBox.Text)); i++)
+            {
+                IdComboBox.Items.Add(i);
             }
         }
     }
